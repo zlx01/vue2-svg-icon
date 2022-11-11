133c15eee87681094a7c30c64c9092e449e5f436
zlx01 <761451281@qq.com>
2022-11-11 16:43:23 +0800

    ios svg问题

--- README.md
+++ README.md
@@ -7,3 +7,21 @@ npm i -D svg-sprite-loader
 ## Reference
 
 * https://juejin.cn/post/6844903517564436493
+
+## svg在chrome显示正常
+
+![chrome](assets/chrome.png)
+
+## svg在safari显示异常
+
+![safari](assets/safari.png)
+
+## svg在本地预览正常
+
+![transfer-in](assets/transfer-in.png)
+
+## svg在本地预览异常
+
+![zrzj](assets/zrzj.png)
+
+结论：部分使用了filter的svg在Safari显示会异常。参考[链接](https://stackoverflow.com/questions/44900980/svg-filter-works-in-chrome-but-not-in-safari-or-firefox)
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,29 @@
     <svg-icon icon-class="kiwi" />
     <svg-icon icon-class="pitaya" />
     <svg-icon icon-class="watermelon" />
+    <hr />
+    <svg-icon icon-class="transfer-in" />
+    <svg-icon icon-class="transfer-out" />
+    <hr />
+    <svg-icon icon-class="zrzj" />
+    <svg-icon icon-class="zczj" />
+    <hr />
+
+    <svg class="blue shape-triangle-3 shape-triangle" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="415" height="455" viewBox="0 0 415 455" data-reactid="48">
+      <defs data-reactid="49">
+        <filter id="shape-triangle" width="182.4%" height="172.4%" x="-41.2%" y="-27.2%" filterUnits="objectBoundingBox" data-reactid="50">
+          <feOffset dy="30" in="SourceAlpha" result="shadowOffsetOuter1" data-reactid="51"></feOffset>
+          <feGaussianBlur in="shadowOffsetOuter1" result="shadowBlurOuter1" stdDeviation="35" data-reactid="52"></feGaussianBlur>
+          <feColorMatrix in="shadowBlurOuter1" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.3 0" data-reactid="53"></feColorMatrix>
+          <feMerge data-reactid="54">
+            <feMergeNode in="shadowMatrixOuter1" data-reactid="55"></feMergeNode>
+            <feMergeNode in="SourceGraphic" data-reactid="56"></feMergeNode></feMerge>
+        </filter>
+      </defs>
+      <g filter="url(#shape-triangle)" fill="none" fill-rule="evenodd" transform="translate(-106 -734)" data-reactid="57">
+        <path class="shape-" d="M170.912 767.496l287.176 74.364a2 2 0 0 1 1.204 2.98l-154.94 253.066a2 2 0 0 1-3.56-.295L168.556 770.18a2 2 0 0 1 2.356-2.685z" data-reactid="58"></path>
+      </g>
+    </svg>
   </div>
 </template>
 
@@ -11,3 +34,10 @@ export default {
   name: "Home",
 };
 </script>
+
+<style>
+.shape-triangle path {
+  fill: blue;
+  color: #F24338;
+}
+</style>
